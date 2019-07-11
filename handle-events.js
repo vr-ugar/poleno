c01381ad64f94262250cfec1e6d12e825c2943b5
= <=>
2019-07-11 23:29:07 +0300

    formatting

--- handle-events.js
+++ handle-events.js
@@ -1,60 +1,65 @@
-	      AFRAME.registerComponent('handle-events', {
-		              update: function () {
-				                var el = this.el;  // <a-sphere>
-				                var flag = false;
-				      	      
-				      		el.addEventListener('mouseenter', () => {
-							
-							console.log("handling mouse enter");
-							flag = true;
+AFRAME.registerComponent('handle-events', {
+	update: function () {
+		var el = this.el;  // <a-sphere>
+		var flag = false;
 
-									if (el.parentEl.getAttribute('visible')) {
-													el.object3D.scale.x = el.object3D.scale.x * 2;
-													el.object3D.scale.y = el.object3D.scale.y * 2;
-													el.object3D.scale.z = el.object3D.scale.z * 2;
-													setTimeout(changeScene, 800);
-												}
-								      });
-				      		el.addEventListener('mouseleave', () => {
-								        flag = false;
-									if (el.parentEl.getAttribute('visible')) {
-													el.object3D.scale.x = el.object3D.scale.x * 0.5;
-													el.object3D.scale.y = el.object3D.scale.y * 0.5;
-													el.object3D.scale.z = el.object3D.scale.z * 0.5;
-												}
-								      });
-				      	      function changeScene() {
-						      		  if (flag) {
-									  			flag = false;
-									  			el.object3D.scale.x = el.object3D.scale.x * 0.5;
-									  			el.object3D.scale.y = el.object3D.scale.y * 0.5;
-									  			el.object3D.scale.z = el.object3D.scale.z * 0.5;
-									              var parel = el.parentEl;
-									  		<!-- get the entity out of the way --> 
-										                  parel.object3D.scale.x = 0;
-									                  parel.object3D.scale.y = 0;
-									                  parel.object3D.scale.z = 0;
-									              var toel = document.getElementById(el.getAttribute('toid'));
-									  		
-									                  toel.object3D.scale = toel.object3D.original_scale;
-									  	    toel.setAttribute('visible', 'true');
-									              
-									              document.getElementById(el.parentEl.id).setAttribute('visible', 'false');
-									  <!-- change ambient sound -->
-										  var ambient = document.getElementById("ambient_sound");
-									  ambient.setAttribute('src', document.getElementById(toel.getAttribute('ambient')).getAttribute('src'));
-									  <!--ambient.setAttribute('src', '#spider_sound');-->
-									  console.log("ambient src");
-									  console.log(ambient.getAttribute("src"));
+		el.addEventListener('mouseenter', () => {
 
-									  <!--mute all scene sounds>
-								          var sounds = parel.parentEl.querySelectorAll('a-sound');
-									  console.log(parel);
-									  console.log("sounds");
-									  console.log(sounds);
-									  		  }
-						      		     };
-				      		    
-				      		};
-		            });
 
+			flag = true;
+
+			if (el.parentEl.getAttribute('visible')) {
+				el.object3D.scale.x = el.object3D.scale.x * 2;
+				el.object3D.scale.y = el.object3D.scale.y * 2;
+				el.object3D.scale.z = el.object3D.scale.z * 2;
+			setTimeout(changeScene, 800);
+			}
+		});
+
+		el.addEventListener('mouseleave', () => {
+			flag = false;
+			if (el.parentEl.getAttribute('visible')) {
+				el.object3D.scale.x = el.object3D.scale.x * 0.5;
+				el.object3D.scale.y = el.object3D.scale.y * 0.5;
+				el.object3D.scale.z = el.object3D.scale.z * 0.5;
+			}
+		});
+
+		function changeScene() {
+			if (flag) {
+				flag = false;
+				el.object3D.scale.x = el.object3D.scale.x * 0.5;
+				el.object3D.scale.y = el.object3D.scale.y * 0.5;
+				el.object3D.scale.z = el.object3D.scale.z * 0.5;
+
+				var parel = el.parentEl;
+
+				<!-- get the entity out of the way --> 
+				parel.object3D.scale.x = 0;
+				parel.object3D.scale.y = 0;
+				parel.object3D.scale.z = 0;
+
+				var toel = document.getElementById(el.getAttribute('toid'));
+
+				toel.object3D.scale = toel.object3D.original_scale;
+				toel.setAttribute('visible', 'true');
+
+				document.getElementById(el.parentEl.id).setAttribute('visible', 'false');
+
+				<!-- change ambient sound -->
+				var ambient = document.getElementById("ambient_sound");
+				ambient.setAttribute('src', document.getElementById(toel.getAttribute('ambient')).getAttribute('src'));
+				<!--ambient.setAttribute('src', '#spider_sound');-->
+				console.log("ambient src");
+				console.log(ambient.getAttribute("src"));
+
+				<!--mute all scene sounds>
+				var sounds = parel.parentEl.querySelectorAll('a-sound');
+				console.log(parel);
+				console.log("sounds");
+				console.log(sounds);
+			}
+		}
+
+	}
+});
