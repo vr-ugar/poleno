04be388556e409dba5dfa1ebdf585e8c5235bb27
= <=>
2019-07-12 00:41:04 +0300

    more portals

--- handle-events.js
+++ handle-events.js
@@ -34,9 +34,15 @@
 										                  parel.object3D.scale.x = 0;
 									                  parel.object3D.scale.y = 0;
 									                  parel.object3D.scale.z = 0;
-									              var toel = document.getElementById(el.getAttribute('toid'));
+									              	
+									              	var toel = document.getElementById(el.getAttribute('toid'));
 									  		
-									                  toel.object3D.scale = toel.object3D.original_scale;
+									                  toel.object3D.scale.x = 1;
+
+									                  toel.object3D.scale.y = 1;
+
+									                  toel.object3D.scale.z = 1;
+									                  
 									  	    toel.setAttribute('visible', 'true');
 									              
 									              document.getElementById(el.parentEl.id).setAttribute('visible', 'false');
